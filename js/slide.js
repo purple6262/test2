b5e4543e1347e86e1d90982c4fe2e16ed35a6ceb
sun-woo <sunwoo12577@gmail.com>
2017-02-08 17:05:50 +0900

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,3 @@
-<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript">
 		$(function(){
 			$("#slide > input:eq(0)").click(function(){
 				$("#slide > div").slideDown(200);
@@ -13,4 +11,3 @@
 				$("#slide > div").slideToggle(200);
 			});			
 		});
-	</script>
